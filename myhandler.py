593f7382c6556d855711078e6bc94f9fc6c45015
Igor Polyakov <IgorPolyakov@users.noreply.github.com>
2017-02-07 19:19:38 +0700

    Update myhandler.py

--- myhandler.py
+++ myhandler.py
@@ -3,35 +3,147 @@ import datetime
 
 def getJsonString():
     res =   [
-		{"object_id": 1,
-		 "object_type": "type1",
-		 "table_model": "table1",
-		 "options": [
-				{"option_type": "option1",
-				 "value": 0.01,
-				 "status": "stat1"
-				},
-				{"option_type": "option2",
-				 "value": 0.02,
-				 "status": "stat2"
-				}
-			    ]
-		},
-		{"object_id": 2,
-		 "object_type": "type2",
-		 "table_model": "model2",
-		 "options": [
-				{"option_type": "option3",
-				 "value": 0.03,
-				 "status": "stat3"
-				},
-				{"option_type": "option4",
-				 "value": 0.04,
-				 "status": "stat4"
-				}
-			    ]
-		}
+	  {
+	    "object_id": "5899b900bd28fec208de5864",
+	    "object_type": "generate",
+	    "table_model": "5899b900dee5abbbe63e4104",
+	    "name": "ROUGHIES",
+	    "description": "Veniam eiusmod dolor esse et. Magna elit aliquip dolore laboris occaecat ut fugiat qui nulla excepteur. Ea minim culpa adipisicing enim enim consectetur enim magna. Exercitation irure ullamco fugiat ea non ullamco enim laboris elit. Laborum elit pariatur nisi nostrud sunt dolore qui eiusmod. Anim ex do dolor eiusmod minim dolore amet mollit irure excepteur dolor.\r\n",
+	    "isActive": true,
+	    "options": [
+	      {
+		"id": 0,
+		"option_type": "ea",
+		"value": 0.5076,
+		"status": false
+	      },
+	      {
+		"id": 1,
+		"option_type": "aliquip",
+		"value": 0.6099,
+		"status": true
+	      },
+	      {
+		"id": 2,
+		"option_type": "labore",
+		"value": 0.8102,
+		"status": true
+	      }
 	    ]
+	  },
+	  {
+	    "object_id": "5899b9007c57c52518b9034a",
+	    "object_type": "eat",
+	    "table_model": "5899b900707c46374d1869d6",
+	    "name": "ULTRASURE",
+	    "description": "Veniam officia labore ex aliquip nisi. Tempor officia nostrud irure excepteur consequat fugiat officia labore consectetur esse. Sunt aliqua magna duis aliquip elit laborum mollit.\r\n",
+	    "isActive": true,
+	    "options": [
+	      {
+		"id": 0,
+		"option_type": "deserunt",
+		"value": 0.7316,
+		"status": false
+	      },
+	      {
+		"id": 1,
+		"option_type": "sint",
+		"value": 0.8913,
+		"status": false
+	      },
+	      {
+		"id": 2,
+		"option_type": "non",
+		"value": 0.976,
+		"status": false
+	      }
+	    ]
+	  },
+	  {
+	    "object_id": "5899b900ff40fa0548ea85d2",
+	    "object_type": "eat",
+	    "table_model": "5899b900b04067f0e409ba03",
+	    "name": "MAINELAND",
+	    "description": "Eu ullamco dolore nisi tempor eu sit quis nisi ipsum id voluptate. Fugiat deserunt culpa occaecat voluptate officia nisi ut. Fugiat laborum labore adipisicing excepteur.\r\n",
+	    "isActive": false,
+	    "options": [
+	      {
+		"id": 0,
+		"option_type": "ad",
+		"value": 0.0261,
+		"status": true
+	      },
+	      {
+		"id": 1,
+		"option_type": "duis",
+		"value": 0.6172,
+		"status": true
+	      },
+	      {
+		"id": 2,
+		"option_type": "magna",
+		"value": 0.891,
+		"status": false
+	      }
+	    ]
+	  },
+	  {
+	    "object_id": "5899b900de66c93c035e2b92",
+	    "object_type": "generate",
+	    "table_model": "5899b9005c113e00d579facb",
+	    "name": "SCHOOLIO",
+	    "description": "Mollit cupidatat sunt duis excepteur commodo ut eu exercitation aute officia id. Voluptate esse sit aliquip qui. Ad aliqua adipisicing laborum aliquip duis sint non qui amet consequat quis commodo. Officia eu proident duis cillum excepteur duis aliqua velit adipisicing. Nostrud duis sit sunt enim sunt sint magna cillum nostrud commodo officia.\r\n",
+	    "isActive": true,
+	    "options": [
+	      {
+		"id": 0,
+		"option_type": "proident",
+		"value": 0.6284,
+		"status": false
+	      },
+	      {
+		"id": 1,
+		"option_type": "quis",
+		"value": 0.7847,
+		"status": false
+	      },
+	      {
+		"id": 2,
+		"option_type": "enim",
+		"value": 0.6826,
+		"status": true
+	      }
+	    ]
+	  },
+	  {
+	    "object_id": "5899b90028824141c0c528a2",
+	    "object_type": "generate",
+	    "table_model": "5899b90021baa9bfd3740dac",
+	    "name": "MARKETOID",
+	    "description": "Esse voluptate adipisicing ex labore. Non dolor labore mollit non eiusmod culpa incididunt incididunt elit tempor non esse Lorem. Sint nostrud est laborum sit officia dolor laboris nulla veniam proident ullamco aliqua.\r\n",
+	    "isActive": true,
+	    "options": [
+	      {
+		"id": 0,
+		"option_type": "labore",
+		"value": 0.0468,
+		"status": false
+	      },
+	      {
+		"id": 1,
+		"option_type": "consectetur",
+		"value": 0.6736,
+		"status": true
+	      },
+	      {
+		"id": 2,
+		"option_type": "cupidatat",
+		"value": 0.2417,
+		"status": false
+	      }
+	    ]
+	  }
+	]
     return json.dumps(res, sort_keys=True, indent=4, separators=(',', ': '))+'\n'
 
 def saveData(data, address):
