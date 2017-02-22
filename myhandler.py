31158a995e18157b309cfb9f914011e5816ef47a
MJ <mariarti91@gmail.com>
2017-02-22 14:35:29 +0600

    upload new version

--- myhandler.py
+++ myhandler.py
@@ -1,177 +1,52 @@
 import json
-import datetime
+from datetime import datetime
+from os import listdir
+from os.path import isfile, join
 
 def getJsonString():
     res =   [
-	  {
-	    "object_id": "5899b900bd28fec208de5864",
-	    "object_type": "generate",
-	    "table_model": "5899b900dee5abbbe63e4104",
-	    "name": "ROUGHIES",
-	    "description": "Veniam eiusmod dolor esse et. Magna elit aliquip dolore laboris occaecat ut fugiat qui nulla excepteur. Ea minim culpa adipisicing enim enim consectetur enim magna. Exercitation irure ullamco fugiat ea non ullamco enim laboris elit. Laborum elit pariatur nisi nostrud sunt dolore qui eiusmod. Anim ex do dolor eiusmod minim dolore amet mollit irure excepteur dolor.\r\n",
-	    "isActive": true,
-	    "options": [
-	      {
-		"id": 0,
-		"option_type": "ea",
-		"value": 0.5076,
-		"status": false
-	      },
-	      {
-		"id": 1,
-		"option_type": "aliquip",
-		"value": 0.6099,
-		"status": true
-	      },
-	      {
-		"id": 2,
-		"option_type": "labore",
-		"value": 0.8102,
-		"status": true
-	      }
+		{"object_id": 1,
+		 "object_type": "type1",
+		 "table_model": "table1",
+		 "options": [
+				{"option_type": "option1",
+				 "value": 0.01,
+				 "status": "stat1"
+				},
+				{"option_type": "option2",
+				 "value": 0.02,
+				 "status": "stat2"
+				}
+			    ]
+		},
+		{"object_id": 2,
+		 "object_type": "type2",
+		 "table_model": "model2",
+		 "options": [
+				{"option_type": "option3",
+				 "value": 0.03,
+				 "status": "stat3"
+				},
+				{"option_type": "option4",
+				 "value": 0.04,
+				 "status": "stat4"
+				}
+			    ]
+		}
 	    ]
-	  },
-	  {
-	    "object_id": "5899b9007c57c52518b9034a",
-	    "object_type": "eat",
-	    "table_model": "5899b900707c46374d1869d6",
-	    "name": "ULTRASURE",
-	    "description": "Veniam officia labore ex aliquip nisi. Tempor officia nostrud irure excepteur consequat fugiat officia labore consectetur esse. Sunt aliqua magna duis aliquip elit laborum mollit.\r\n",
-	    "isActive": true,
-	    "options": [
-	      {
-		"id": 0,
-		"option_type": "deserunt",
-		"value": 0.7316,
-		"status": false
-	      },
-	      {
-		"id": 1,
-		"option_type": "sint",
-		"value": 0.8913,
-		"status": false
-	      },
-	      {
-		"id": 2,
-		"option_type": "non",
-		"value": 0.976,
-		"status": false
-	      }
-	    ]
-	  },
-	  {
-	    "object_id": "5899b900ff40fa0548ea85d2",
-	    "object_type": "eat",
-	    "table_model": "5899b900b04067f0e409ba03",
-	    "name": "MAINELAND",
-	    "description": "Eu ullamco dolore nisi tempor eu sit quis nisi ipsum id voluptate. Fugiat deserunt culpa occaecat voluptate officia nisi ut. Fugiat laborum labore adipisicing excepteur.\r\n",
-	    "isActive": false,
-	    "options": [
-	      {
-		"id": 0,
-		"option_type": "ad",
-		"value": 0.0261,
-		"status": true
-	      },
-	      {
-		"id": 1,
-		"option_type": "duis",
-		"value": 0.6172,
-		"status": true
-	      },
-	      {
-		"id": 2,
-		"option_type": "magna",
-		"value": 0.891,
-		"status": false
-	      }
-	    ]
-	  },
-	  {
-	    "object_id": "5899b900de66c93c035e2b92",
-	    "object_type": "generate",
-	    "table_model": "5899b9005c113e00d579facb",
-	    "name": "SCHOOLIO",
-	    "description": "Mollit cupidatat sunt duis excepteur commodo ut eu exercitation aute officia id. Voluptate esse sit aliquip qui. Ad aliqua adipisicing laborum aliquip duis sint non qui amet consequat quis commodo. Officia eu proident duis cillum excepteur duis aliqua velit adipisicing. Nostrud duis sit sunt enim sunt sint magna cillum nostrud commodo officia.\r\n",
-	    "isActive": true,
-	    "options": [
-	      {
-		"id": 0,
-		"option_type": "proident",
-		"value": 0.6284,
-		"status": false
-	      },
-	      {
-		"id": 1,
-		"option_type": "quis",
-		"value": 0.7847,
-		"status": false
-	      },
-	      {
-		"id": 2,
-		"option_type": "enim",
-		"value": 0.6826,
-		"status": true
-	      }
-	    ]
-	  },
-	  {
-	    "object_id": "5899b90028824141c0c528a2",
-	    "object_type": "generate",
-	    "table_model": "5899b90021baa9bfd3740dac",
-	    "name": "MARKETOID",
-	    "description": "Esse voluptate adipisicing ex labore. Non dolor labore mollit non eiusmod culpa incididunt incididunt elit tempor non esse Lorem. Sint nostrud est laborum sit officia dolor laboris nulla veniam proident ullamco aliqua.\r\n",
-	    "isActive": true,
-	    "options": [
-	      {
-		"id": 0,
-		"option_type": "labore",
-		"value": 0.0468,
-		"status": false
-	      },
-	      {
-		"id": 1,
-		"option_type": "consectetur",
-		"value": 0.6736,
-		"status": true
-	      },
-	      {
-		"id": 2,
-		"option_type": "cupidatat",
-		"value": 0.2417,
-		"status": false
-	      }
-	    ]
-	  }
-	]
-    return json.dumps(res, sort_keys=True, indent=4, separators=(',', ': '))+'\n'
+    return json.dumps(res)
 
-def saveData(data, address):
-    fName = "{}_{}".format(address[0], datetime.datetime.now().strftime("%H:%M:%S_%d.%m.%Y"))
-    with open("./data/"+fName, "w") as f:
-	f.write(data)
-    return fName
-
-def handle(connection, address):
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger("process-%r" % (address,))
-    try:
-	logger.debug("Connected %r at %r", connection, address)
-	connection.sendall(getJsonString())
-	while True:
-	    data = connection.recv(1024)
-	    if data:
-		logger.debug("Have a new data from the Client!")
-		logger.debug("Data is saved in '%r' file", saveData(data, address))
-		logger.debug("Sending back data")
-		connection.sendall(data)
-	    else:
-		logger.debug("No data from the Client")
-		break
-    except:
-	logger.exception("Problem handling request")
-    finally:
-	logger.debug("Closing socket")
-	connection.close()
+def saveData(data, name):
+	with open("./data/"+name.replace('/', '_'), "wb") as f:
+		f.write(data)
 	return
+
+def getData(path):
+	with open("./data/"+path.replace('/', '_'), "rb") as f:
+		return f.read()
+
+def getList():
+	result = "<http><body>"
+	result += "".join(["<p><a href=\"http://82.117.171.124:9099{0}\">{0}</a></p>".format(i.replace('_', '/')) for i in listdir("./data/") if isfile(join("./data/", i))])
+	result += "</body></http>"
+	return result
--- service.py
+++ service.py
@@ -1,20 +1,56 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import server
-import multiprocessing
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import myhandler
 
-if __name__ == '__main__':
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    server = server.Server("0.0.0.0", 9099)
-    try:
-	logging.info("Listening...")
-	server.start()
-    except:
-	logging.exception("Starting server error!")
-    finally:
-	logging.info("Shutting down")
-	for process in multiprocessing.active_children():
-	    process.terminate()
-	    process.join()
-    logging.info("All done")
+antiPost = ['/', '/list']
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+	def setup(self):
+		BaseHTTPRequestHandler.setup(self)
+		self.request.settimeout(10)
+
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.send_header('Access-Control-Allow-Origin','*')
+		self.end_headers()
+
+		if self.path == '/':
+			message = bytes(myhandler.getJsonString(), "utf8")
+		elif self.path == '/list':
+			message = bytes(myhandler.getList(), "utf8")
+		else:
+			try:
+				message =  myhandler.getData(self.path)
+			except:
+				message = bytes("", "utf8")
+		
+		self.wfile.write(message)
+		return
+
+	def do_POST(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.send_header('Access-Control-Allow-Origin','*')
+		self.end_headers()
+		if not self.path in antiPost  :
+			content_len = int(self.headers['Content-Length'])
+			myhandler.saveData(self.rfile.read(content_len), self.path)
+		return;
+
+	def do_OPTIONS(self):
+		self.send_response(200)
+		self.send_header('Access-Control-Allow-Origin','*')
+		self.send_header('Access-Control-Allow-Methods','GET, POST, OPTIONS')
+		self.end_headers()
+		return;
+
+def run():
+	print("starting server...")
+	server_address = ('', 9099)
+	httpd = HTTPServer(server_address, MyHTTPRequestHandler)
+	print("running server...")
+	httpd.serve_forever()
+
+run()
