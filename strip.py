7af7c41e5c4bf42afdbdfa76e5369c111dafb5a5
MJ <mariarti91@gmail.com>
2017-02-24 20:07:25 +0700

    some changes in strip lib

--- strip.py
+++ strip.py
@@ -1,13 +1,13 @@
 import http.client
 
-server_url = 'localhost:9099'
+server_url = 'ascae.ru:9099'
 
 def doGET(url):
 	conn = http.client.HTTPConnection(server_url)
 	conn.request('GET', url)
 	response = conn.getresponse()
 	conn.close()
-	return response.read() if response.status == 200 else ""
+	return response.read().decode('utf8') if response.status == 200 else ""
 
 def doPOST(url, data):
 	conn = http.client.HTTPConnection(server_url)
