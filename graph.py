beed24044f1b1f7a69d6b6de3507cf21a06a0c46
MJ <mariarti91@gmail.com>
2017-03-23 16:26:05 +0700

    change labels

--- graph.py
+++ graph.py
@@ -63,38 +63,38 @@ class GraphMaker:
 		buf['color_node'] = 'red' #self.getNodeColor(data) 
 		if 'MS' in data['Ident']:
 			buf['type'] = "electric_substaion"
-			buf['label'] = "\u042d\u043b\u0435\u043a\u0442\u0440\u0438\u0447\u0435\u0441\u043a\u0430\u044f \u0441\u0442\u0430\u043d\u0446\u0438\u044f"
+			buf['label'] = "Подстанция"
 		elif 'SS' in data['Ident']:
 			buf['type'] = "mini_electric_substaion"
-			buf['label'] = "\u042d\u043b\u0435\u043a\u0442\u0440\u0438\u0447\u0435\u0441\u043a\u0430\u044f \u043f\u043e\u0434\u0441\u0442\u0430\u043d\u0446\u0438\u044f"
+			buf['label'] = "Миниподстанция"
 		elif 'GW' in data['Ident']:
 			buf['type'] = "wind"
-			buf['label'] = "\u0412\u0435\u0442\u0440\u043e\u0433\u0435\u043d\u0435\u0440\u0430\u0442\u043e\u0440 " + data['Ident'].split('_')[-1]
+			buf['label'] = "\u0412\u0435\u0442\u0440\u043e\u0433\u0435\u043d\u0435\u0440\u0430\u0442\u043e\u0440 #" + data['Ident'].split('_')[-1]
 		elif 'GS' in data['Ident']:
 			buf['type'] = 'sun'
-			buf['label'] = "\u0421\u043e\u043b\u043d\u0435\u0447\u043d\u0430\u044f \u0431\u0430\u0442\u0430\u0440\u0435\u044f " + data['Ident'].split('_')[-1]
+			buf['label'] = "\u0421\u043e\u043b\u043d\u0435\u0447\u043d\u0430\u044f \u0431\u0430\u0442\u0430\u0440\u0435\u044f #" + data['Ident'].split('_')[-1]
 		elif 'GRD' in data['Ident']:
 			buf['type'] = 'disel'
-			buf['label'] = "\u0414\u0438\u0437\u0435\u043b\u044c \u0433\u0435\u043d\u0435\u0440\u0430\u0442\u043e\u0440 " + data['Ident'].split('_')[-1]
+			buf['label'] = "\u0414\u0438\u0437\u0435\u043b\u044c \u0433\u0435\u043d\u0435\u0440\u0430\u0442\u043e\u0440 #" + data['Ident'].split('_')[-1]
 		elif 'GRA' in data['Ident']:
 			buf['type'] = 'accumulate'
-			buf['label'] = "\u042d\u043b\u0435\u043a\u0442\u0440\u0438\u0447\u0435\u0441\u043a\u0438\u0439 \u0430\u043a\u043a\u0443\u043c\u0443\u043b\u044f\u0442\u043e\u0440_" + data['Ident'].split('_')[-1]
+			buf['label'] = "\u042d\u043b\u0435\u043a\u0442\u0440\u0438\u0447\u0435\u0441\u043a\u0438\u0439 \u0430\u043a\u043a\u0443\u043c\u0443\u043b\u044f\u0442\u043e\u0440 #" + data['Ident'].split('_')[-1]
 		elif 'CH' in data['Ident']:
 			buf['type'] = 'hospital'
-			buf['label'] = "\u0413\u043e\u0441\u043f\u0438\u0442\u0430\u043b\u044c " + data['Ident'].split('_')[-1]	
+			buf['label'] = "Больница #" + data['Ident'].split('_')[-1]	
 		elif 'CU' in data['Ident']:
 			buf['type'] = 'district'
-			buf['label'] = "\u041c\u0438\u043a\u0440\u043e\u0440\u0430\u0439\u043e\u043d " + data['Ident'].split('_')[-1]	
+			buf['label'] = "\u041c\u0438\u043a\u0440\u043e\u0440\u0430\u0439\u043e\u043d #" + data['Ident'].split('_')[-1]	
 		elif 'CF' in data['Ident']:
 			buf['type'] = 'factory'
-			buf['label'] = "\u0424\u0430\u0431\u0440\u0438\u043a\u0430 " + data['Ident'].split('_')[-1]
+			buf['label'] = "\u0424\u0430\u0431\u0440\u0438\u043a\u0430 #" + data['Ident'].split('_')[-1]
 		elif 'SN' in data['Ident']:
 			buf['type'] = 'stick'
 			buf['color_node'] = '#1ab394'
 			buf['label'] = ""	
 		else:
 			buf['type'] = 'unknown'
-			buf['label'] = 'хз чо это'
+			buf['label'] = 'неопределено'
 		node = {'data': buf}
 		self.nodes[buf['id']] = node
 		return node
